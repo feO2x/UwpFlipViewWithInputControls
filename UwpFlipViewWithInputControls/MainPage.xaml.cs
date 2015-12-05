f5e5cd876ef7fb8f31a9a86cb33e54378a488199
Kenny Pflug <kenny.pflug@live.de>
2015-12-05 21:31:05 +0100

    added initial UWP project

---
+++ UwpFlipViewWithInputControls/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace UwpFlipViewWithInputControls
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
